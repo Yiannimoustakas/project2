299411882e16cf30b3b1936d5886c9bd9a95d6c8
Jonathan Odlum <jonathan.odlum@gmail.com>
2018-11-02 15:25:19 +1100

    testing routes mergeing

---
+++ backend-project2/app/controllers/solutions_controller.rb
@@ -0,0 +1,29 @@
+class SolutionsController < ApplicationController
+  def new
+
+  end
+
+  def create
+
+  end
+
+  def edit
+
+  end
+
+  def update
+
+  end
+
+  def index
+
+  end
+
+  def show
+
+  end
+
+  def destroy
+    
+  end
+end
