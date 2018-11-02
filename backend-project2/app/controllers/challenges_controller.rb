9fe854a9fc4b40419129998d1a4454775f7e491c
Yianni Moustakas <yianni.m13@gmail.com>
2018-11-02 15:24:20 +1100

    backend challenges and test pairs controllers

---
+++ backend-project2/app/controllers/challenges_controller.rb
@@ -0,0 +1,22 @@
+class ChallengesController < ApplicationController
+  def create
+  end
+
+  def update
+  end
+
+  def new
+  end
+
+  def edit
+  end
+
+  def index
+  end
+
+  def destroy
+  end
+
+  def show
+  end
+end
---
+++ backend-project2/app/controllers/test_pairs_controller.rb
@@ -0,0 +1,22 @@
+class TestPairsController < ApplicationController
+  def create
+  end
+
+  def update
+  end
+
+  def new
+  end
+
+  def edit
+  end
+
+  def index
+  end
+
+  def destroy
+  end
+
+  def show
+  end
+end
