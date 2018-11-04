1a79d62a0807feedbd74bd94b1648ae89e9c0601
Jonathan Odlum <jonathan.odlum@gmail.com>
2018-11-05 09:21:50 +1100

    merging

--- backend-project2/app/controllers/solutions_controller.rb
+++ backend-project2/app/controllers/solutions_controller.rb
@@ -1,18 +1,29 @@
 class SolutionsController < ApplicationController
+
+  skip_before_action :verify_authenticity_token
+
   def new
-    @solution = Solution.create
+    # route to here from a challenge show page, so challenge id will be in params.
+    @challenge_id = Challenge.first
+    @solution = Solution.new
+    @code_snippet = 'console.log(str)'
   end
 
   def create
-
+    solution = Solution.new solution_params
+    solution.user_id = User.first.id
+    raise 'hell'
   end
 
   def edit
-    @solution = Solution.find(params[:id])
+    @solution = Solution.first
   end
 
   def update
-
+    solution = Solution.find(params[:id])
+    solution.update(
+      solution_params
+    )
   end
 
   def index
@@ -26,5 +37,11 @@ class SolutionsController < ApplicationController
   def destroy
     solution = Solution.find(params[:id])
     solution.destroy()
+  end
+
+  private
+
+  def solution_params
+    params.permit(:code, :challenge_id)
   end
 end
--- backend-project2/db/seeds.rb
+++ backend-project2/db/seeds.rb
@@ -14,3 +14,27 @@ u2 = User.create name: 'haxor', email: 'haxor@gmail.com', password: 'chicken'
 u3 = User.create name: 'noobcoder', email: 'noobcoder@gmail.com', password: 'chicken'
 
 puts "Created #{User.all.length} users."
+
+
+Challenge.destroy_all
+
+c1 = Challenge.create name: 'Square number', description: "Return the square of the given input", user_id: u1.id
+
+puts "Created #{Challenge.all.length} Challenges."
+
+TestPair.destroy_all
+
+tp1 = TestPair.create input: 1, output: 1, challenge_id: c1.id
+tp2 = TestPair.create input: 2, output: 4, challenge_id: c1.id
+tp3 = TestPair.create input: 3, output: 9, challenge_id: c1.id
+tp4 = TestPair.create input: 4, output: 16, challenge_id: c1.id
+tp5 = TestPair.create input: 5, output: 25, challenge_id: c1.id
+tp6 = TestPair.create input: 6, output: 36, challenge_id: c1.id
+
+puts "Created #{TestPair.all.length} TestPairs."
+
+Solution.destroy_all
+
+s1 = Solution.create code: "return (x * x);", challenge_id: c1.id, user_id: u2.id
+
+puts "Created #{Solution.all.length} Solutions."
