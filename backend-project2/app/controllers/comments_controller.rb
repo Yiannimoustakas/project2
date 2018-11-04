9c520051a48ac7f268e30c557d73c8d96b51f864
Tom Hunt <tomhunt794@gmail.com>
2018-11-05 09:11:44 +1100

    update

---
+++ backend-project2/app/controllers/comments_controller.rb
@@ -0,0 +1,19 @@
+class CommentsController < ApplicationController
+  def show
+  end
+
+  def index
+  end
+
+  def update
+  end
+
+  def edit
+  end
+
+  def new
+  end
+
+  def create
+  end
+end
--- backend-project2/config/routes.rb
+++ backend-project2/config/routes.rb
@@ -1,5 +1,11 @@
 Rails.application.routes.draw do
 
+  get 'comments/show'
+  get 'comments/index'
+  get 'comments/update'
+  get 'comments/edit'
+  get 'comments/new'
+  get 'comments/create'
   get 'session/new'
 
   get 'session/create'
